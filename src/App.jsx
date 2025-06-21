887ef44db71aec5443958b78ce56c6fdf51f3f27
Amxnnn <amankamboj6164@gmail.com>
2025-06-21 15:07:12 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import gsap from "gsap"
+import { ScrollTrigger, SplitText } from "gsap/all"
+
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
+const App = () => {
+  return (
+    <div>
+      hii
+    </div>
+  )
+}
+
+export default App
