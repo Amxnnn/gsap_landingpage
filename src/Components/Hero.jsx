b52532f332fd8aa77e48f3506f688bdbc746b0c1
Amxnnn <amankamboj6164@gmail.com>
2025-06-22 13:33:09 +0530

    Hero Scroll

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,12 +1,15 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 import { SplitText } from "gsap/all"
+import { useRef } from "react"
+import { useMediaQuery } from "react-responsive"
 
 const Hero = () => {
+  const videoRef = useRef()
+  const isMobile = useMediaQuery({maxWidth: 767})
   useGSAP(()=>{
     const heroSplit = new SplitText('.title',{type:'chars,words'})
     const paragraphSplit = new SplitText('.subtitle',{type:'lines'})
-
     heroSplit.chars.forEach((char)=>char.classList.add('text-gradient'))
 
     gsap.from(heroSplit.chars,{
@@ -37,6 +40,28 @@ const Hero = () => {
     }).to('.right-leaf',{
       y:200
     },0).to('.left-leaf',{y:-200},0)
+    // first thing is for element and second is for screen 
+    const startValue = isMobile?'top 50%' : 'center 60%'
+    const endValue =isMobile?'120% top' : 'bottom top'
+
+
+    const tl = gsap.timeline({
+      scrollTrigger:{
+        trigger:'video',
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true
+      }
+    }) 
+
+    videoRef.current.onloadedmetadata = ()=>{
+      tl.to(videoRef.current,{
+        currentTime: videoRef.current.duration
+      })
+    }
+
+
 
   },[])
   return (
@@ -67,7 +92,18 @@ const Hero = () => {
             </div>
           </div>
         </div>
-      </section>      
+      </section>
+   
+      <div className="video absolute inset-0" >
+        <video
+        ref={videoRef}
+        src="\videos\output.mp4"
+        muted
+        // hide video track sound length etc 
+        playsInline
+        preload="auto"
+        />
+      </div> 
     </>
   )
 }
